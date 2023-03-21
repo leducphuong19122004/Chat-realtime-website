1bb616646c0a3c4ce01b11889ad2b5c87da6a3ea
Le_Duc_Phuong <phuongtroc2004@gmail.com>
2023-03-21 18:16:11 +0700

    add env variable

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -3,11 +3,11 @@ import mysql from 'mysql2';
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.MYSQLHOST || 'localhost',
+    user: MYSQLUSER || 'root',
     password: '19122004',
-    database: 'databasenodejs',
-    port: '3036'
+    database: MYSQLPASSWORD || 'databasenodejs',
+    port: MYSQLPORT || '3036'
 }).promise(); // mysql2 cung cấp .promise() function giúp đảm bảo code đc chạy theo đúng trình tự 
 
 
